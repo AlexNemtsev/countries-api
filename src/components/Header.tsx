3de45ead53d9460068deb69a5e675c6b06903786
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-04-20 07:47:32 +0300

    feat: header is done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,16 +8,33 @@ const HeaderEl = styled.header`
   background-color: var(--colors-ui-base);
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 
 const Title = styled.a.attrs({
   href: '/',
-})``;
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
 
-const ModeSwitcher = styled.div``;
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-transform: capitalize;
+  cursor: pointer;
+`;
 
 export default function Header() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  const toggleTheme = () => (theme === 'light' ? setTheme('dark') : setTheme('light'));
 
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
@@ -28,8 +45,9 @@ export default function Header() {
       <Container>
         <Wrapper>
           <Title>Where is the world?</Title>
-          <ModeSwitcher>
-            <IoMoon /> Light theme
+          <ModeSwitcher onClick={toggleTheme}>
+            {theme === 'light' ? <IoMoonOutline size="14px" /> : <IoMoon size="14px" />}
+            <span style={{ marginLeft: '0.75rem' }}>{theme} theme</span>
           </ModeSwitcher>
         </Wrapper>
       </Container>
