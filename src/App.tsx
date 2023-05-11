85918c37db9b61f6632066ce5b121185313459f5
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-05-11 13:35:26 +0300

    refactor: add modules for header and info

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { Header } from './components/Header';
 import Main from './components/Main';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,87 +1,8 @@
-import styled from 'styled-components';
+import styles from './Info.module.scss';
 import { Link } from 'react-router-dom';
 import { CountryInfo } from '../interfaces/country-info';
 import { useState, useEffect } from 'react';
 import { filterByCodes } from '../libs/loader';
-
-const Wrapper = styled.section`
-  margin-top: 3rem;
-  width: 100%;
-  display: grid;
-  grid-template-columns: 100%;
-  gap: 2rem;
-
-  @media (min-width: 767px) {
-    grid-template-columns: minmax(100px, 400px) 1fr;
-    align-items: center;
-    gap: 5rem;
-  }
-  @media (min-width: 1024px) {
-    grid-template-columns: minmax(400px, 600px) 1fr;
-  }
-`;
-
-const InfoImage = styled.img`
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-`;
-
-const InfoTitle = styled.h1`
-  margin: 0;
-  font-weight: var(--fw-normal);
-`;
-
-const ListGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-
-  @media (min-width: 1024px) {
-    gap: 4rem;
-    flex-direction: row;
-  }
-`;
-
-const List = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const ListItem = styled.li`
-  line-height: 1.8;
-`;
-
-const Meta = styled.div`
-  margin-top: 3rem;
-  display: flex;
-  gap: 1.5rem;
-  flex-direction: column;
-  align-items: flex-start;
-
-  @media (min-width: 767px) {
-    flex-direction: row;
-    align-items: center;
-  }
-`;
-
-const TagGroup = styled.div`
-  display: flex;
-  gap: 1rem;
-  flex-wrap: wrap;
-`;
-
-const Tag = styled(Link)`
-  padding: 0 1rem;
-  background-color: var(--colors-ui-base);
-  box-shadow: var(--shadow);
-  line-height: 1.5;
-  cursor: pointer;
-  text-decoration: none;
-  color: var(--colors-text);
-`;
 
 export const Info = (props: CountryInfo) => {
   const commonName = props.name.common;
@@ -101,60 +22,60 @@ export const Info = (props: CountryInfo) => {
   }, [borders]);
 
   return (
-    <Wrapper>
-      <InfoImage src={flags.svg} alt={flags.alt} />
+    <section className={styles.section}>
+      <img src={flags.svg} alt={flags.alt} className={styles.infoImage} />
       <div>
-        <InfoTitle>{commonName}</InfoTitle>
-        <ListGroup>
-          <List>
-            <ListItem>
+        <h1 className={styles.infoTitle}>{commonName}</h1>
+        <div className={styles.listGroup}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
               <b>Native Name: </b> {nativeName}
-            </ListItem>
-            <ListItem>
+            </li>
+            <li className={styles.listItem}>
               <b>Population: </b> {population}
-            </ListItem>
-            <ListItem>
+            </li>
+            <li className={styles.listItem}>
               <b>Region: </b> {region}
-            </ListItem>
-            <ListItem>
+            </li>
+            <li className={styles.listItem}>
               <b>Sub Region: </b> {subregion}
-            </ListItem>
-            <ListItem>
+            </li>
+            <li className={styles.listItem}>
               <b>Capital{capital.length > 1 ? 's' : ''}: </b> <span>{capital.join(', ')}</span>
-            </ListItem>
-          </List>
-          <List>
+            </li>
+          </ul>
+          <ul className={styles.list}>
             {tld && (
-              <ListItem>
+              <li className={styles.listItem}>
                 <b>Top Level Domain{tld.length > 1 ? 's' : ''}: </b>
                 <span>{tld.join(', ')}</span>
-              </ListItem>
+              </li>
             )}
-            <ListItem>
+            <li className={styles.listItem}>
               <b>Currenc{currenciesList.length > 1 ? 'ies' : 'y'}: </b>
               <span>{currenciesList.map((code) => currencies[code].name).join(', ')}</span>
-            </ListItem>
-            <ListItem>
+            </li>
+            <li className={styles.listItem}>
               <b>Languag{languagesList.length > 1 ? 'es' : 'e'}: </b>
               <span>{languagesList.map((code) => languages[code]).join(', ')}</span>
-            </ListItem>
-          </List>
-        </ListGroup>
-        <Meta>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.meta}>
           <b>Border countries: </b>
           {!neighbours.length ? (
             <span>There are no border countries</span>
           ) : (
-            <TagGroup>
+            <div className={styles.tagGroup}>
               {neighbours.map((border) => (
-                <Tag key={border} to={'/country/' + border}>
+                <Link key={border} to={'/country/' + border} className={styles.tag}>
                   {border}
-                </Tag>
+                </Link>
               ))}
-            </TagGroup>
+            </div>
           )}
-        </Meta>
+        </div>
       </div>
-    </Wrapper>
+    </section>
   );
 };
