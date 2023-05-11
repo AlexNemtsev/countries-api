7e4371203741009b883650d4ce52ac44e1b2be52
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-05-11 11:43:05 +0300

    refactor: add css modules for back button and container

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,20 +1,16 @@
-import styled from 'styled-components';
+import styles from './BackButton.module.scss';
 
-const BackButton = styled.button`
-  padding: 0 1rem;
-  background-color: var(--colors-ui-base);
-  box-shadow: var(--shadow);
-  line-height: 2.5;
-  border-radius: var(--radius);
-  display: flex;
-  align-items: center;
-  gap: 0.75rem;
-  color: var(--color-text);
-  text-decoration: none;
-  width: 96px;
-  border: none;
-  outline: none;
-  cursor: pointer;
-`;
+interface BackButtonProps {
+  children?: React.ReactNode;
+  onClick: () => void;
+}
+
+export const BackButton = ({ children, onClick }: BackButtonProps) => {
+  return (
+    <button className={styles.BackButton} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
 export default BackButton;
